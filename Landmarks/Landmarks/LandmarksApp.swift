1caa0eba803983e440e8a37033a5bbfad3e692e3
chubbyhippo <mkmsacc@outlook.com>
2024-10-04 22:19:21 +0700

    Add project setup for Landmarks app
    Initialized Xcode project with basic app structure. Includes main app file, content view, assets, unit tests, and UI tests. Essential configurations and default assets have been added.

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by nt on 4/10/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
