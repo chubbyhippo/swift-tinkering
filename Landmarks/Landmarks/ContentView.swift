974e8e148315ffe6d8cdfe2d7f503826226d6b2a
chubbyhippo <mkmsacc@outlook.com>
2024-10-05 21:39:54 +0700

    Change font styles

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello SwiftUI!")
+        Text("Hello, SwiftUI!")
+            .font(.title)
+            .foregroundColor(.green)
+            .fontWeight(.heavy)
     }
 }
 
