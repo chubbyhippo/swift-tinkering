9e0482f4c6f0ed9b399f8a6bf282186902948f91
chubbyhippo <mkmsacc@outlook.com>
2024-10-06 21:49:41 +0700

    Add MapView SwiftUI component
    Introduce a new `MapView.swift` file with a basic SwiftUI view that displays a "Hello, World!" text. This is a foundational step for incorporating map functionalities within the Landmarks app.

---
+++ Landmarks/Landmarks/MapView.swift
@@ -0,0 +1,19 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by nt on 6/10/2567 BE.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
