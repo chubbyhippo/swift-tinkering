ae091dcf61222ba9578144a69de5c9927b617a06
chubbyhippo <mkmsacc@outlook.com>
2024-10-06 21:12:38 +0700

    Update ContentView to display "Turtle Rock" in a VStack
    Changed the ContentView to use a VStack instead of directly displaying text. This allows for easier future UI enhancements and better organization of view components.

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -9,10 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, SwiftUI!")
-            .font(.title)
-            .foregroundColor(.green)
-            .fontWeight(.heavy)
+        VStack {
+            Text("Turtle Rock")
+                .font(.title)
+        }
     }
 }
 
