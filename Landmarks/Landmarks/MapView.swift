05a09ae7194b11189a3d3df27bfbd4e59a13daf9
chubbyhippo <mkmsacc@outlook.com>
2024-10-06 22:02:15 +0700

    Optimize import statements in MapView.swift
    Reorganized import statements for clarity and merged related imports. Adjusted formatting for better readability in the region property.

--- Landmarks/Landmarks/MapView.swift
+++ Landmarks/Landmarks/MapView.swift
@@ -5,17 +5,18 @@
 //  Created by nt on 6/10/2567 BE.
 //
 
-import SwiftUI
 import MapKit
+import SwiftUI
 
 struct MapView: View {
     var body: some View {
         Map(initialPosition: .region(region))
     }
-    
+
     private var region: MKCoordinateRegion {
         MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+            center: CLLocationCoordinate2D(
+                latitude: 34.011_286, longitude: -116.166_868),
             span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
         )
     }
